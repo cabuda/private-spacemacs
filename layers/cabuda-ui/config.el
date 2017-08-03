b5d5aa491ae38587e494a3d7a0a1dbe1357c696e
duxinlong <duxinlong@baidu.com>
2017-08-03 23:28:17 +0800

    open linum in prog-mode

---
+++ layers/cabuda-ui/config.el
@@ -0,0 +1,42 @@
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;; http://stackoverflow.com/questions/3875213/turning-on-linum-mode-when-in-python-c-mode
+(setq linum-mode-inhibit-modes-list '(eshell-mode
+                                      shell-mode
+                                      profiler-report-mode
+                                      ffip-diff-mode
+                                      dictionary-mode
+                                      erc-mode
+                                      browse-kill-ring-mode
+                                      etags-select-mode
+                                      dired-mode
+                                      help-mode
+                                      text-mode
+                                      fundamental-mode
+                                      jabber-roster-mode
+                                      jabber-chat-mode
+                                      inferior-js-mode
+                                      inferior-python-mode
+                                      inferior-scheme-mode
+                                      twittering-mode
+                                      compilation-mode
+                                      weibo-timeline-mode
+                                      woman-mode
+                                      Info-mode
+                                      calc-mode
+                                      calc-trail-mode
+                                      comint-mode
+                                      gnus-group-mode
+                                      inf-ruby-mode
+                                      gud-mode
+                                      org-mode
+                                      org-agenda-mode
+                                      vc-git-log-edit-mode
+                                      log-edit-mode
+                                      term-mode
+                                      spacemacs-buffer-mode
+                                      w3m-mode
+                                      speedbar-mode
+                                      gnus-summary-mode
+                                      gnus-article-mode
+                                      calendar-mode))
