e0bb12ffe5edde682e540809793ffc0f76b581fd
duxinlong <cabuda@outlook.com>
2017-09-21 21:39:17 +0800

    close linum-relative

--- layers/cabuda-ui/packages.el
+++ layers/cabuda-ui/packages.el
@@ -1,11 +1,11 @@
 (setq cabuda-ui-packages
       (append '(
-                linum-relative
+                ;; linum-relative
                 ) cabuda-themes))
 
 ;; define programmatically the init functions
 (dolist (pkg cabuda-themes)
   (eval `(defun ,(intern (format "cabuda-ui/init-%S" pkg)) nil)))
 
-(defun cabuda-ui/post-init-linum-relative ()
-  (linum-relative-on))
+;; (defun cabuda-ui/post-init-linum-relative ()
+;;   (linum-relative-on))
