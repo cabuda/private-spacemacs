d382cebb26b8d86129836448835dcea4468c6c78
duxinlong <duxinlong@baidu.com>
2017-09-27 15:35:58 +0800

    close linum on startup

--- layers/cabuda-ui/funcs.el
+++ layers/cabuda-ui/funcs.el
@@ -1,5 +1,5 @@
-(defun linum-on-inhibit-for-modes (orig-fun &rest args)
-  (unless (member major-mode linum-mode-inhibit-modes-list)
-    (apply orig-fun args)))
-(advice-add 'linum-on :around #'linum-on-inhibit-for-modes)
+;; (defun linum-on-inhibit-for-modes (orig-fun &rest args)
+;;   (unless (member major-mode linum-mode-inhibit-modes-list)
+;;     (apply orig-fun args)))
+;; (advice-add 'linum-on :around #'linum-on-inhibit-for-modes)
 
